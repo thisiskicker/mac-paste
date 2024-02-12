f1201b6b1cc230adbb9c1b5e20a66367c84cb71d
thisiskicker <thisiskicker@users.noreply.github.com>
2024-02-12 16:37:06 -0600

    Create paste.js

---
+++ paste.js
@@ -0,0 +1,12 @@
+//this is a node.js script that will paste the contents of the clipboard as keystrokes using nut.js
+//this is useful for pasting into applications that don't support pasting from the clipboard
+
+const nut = require('@nut-tree/nut-js');
+
+async function paste() {
+    await new Promise(resolve => setTimeout(resolve, 4000)); // sleep for 4 seconds
+    const text = await nut.clipboard.getContent();
+    await nut.keyboard.type(text);
+}
+
+paste();
